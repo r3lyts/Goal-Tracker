d8ea5ae8617a63d8caccf7b920302e09adf28eef
Tyler Small <r3lyts@icloud.com>
2024-12-25 17:15:49 -0500

    Initial Commit

---
+++ Goal Tracker/Goal_TrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Goal_TrackerApp.swift
+//  Goal Tracker
+//
+//  Created by Tyler Small on 12/25/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Goal_TrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Goal TrackerTests/Goal_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_TrackerTests.swift
+//  Goal TrackerTests
+//
+//  Created by Tyler Small on 12/25/24.
+//
+
+import Testing
+@testable import Goal_Tracker
+
+struct Goal_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
